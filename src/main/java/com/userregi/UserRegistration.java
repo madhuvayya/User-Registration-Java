8a6618015830bf13ebefaa1c0b83b4d1825a6a9e
madhuvayya <madhu.vayya@gmail.com>
2020-04-30 09:13:18 +0530

    Modified email regex pattern to pass all test case

--- src/main/java/com/userregi/UserRegistration.java
+++ src/main/java/com/userregi/UserRegistration.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     private static final String NAME_PATTERN = "^[A-Z][a-z]{2,}";
-    private static final String EMAIL_PATTERN = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+[.][a-zA-Z]{2,4}([.][a-z]{2})?$";
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$";
     private static final String MOB_NUM_PATTERN = "^[0-9]{2}[ ]*[6-9][0-9]{9}$";
     private static final String PASSWORD_PATTERN1 = "(?=.*[A-Z]+)(?=.*[0-9]+).{8,}";
     private static final String PASSWORD_PATTERN2 = "[0-9a-zA-Z]*[^0-9a-zA-Z][0-9a-zA-Z]*";
